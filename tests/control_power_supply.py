b5767587b2f7ce9a6b3ca4f12982f01d149d89b4
Andrei Faur <da3drus@gmail.com>
2023-09-09 18:46:40 +0200

    Rework pipeline to use new power supply
    - Add Python script for controlling the power supply and use that script
      in the pipeline
    - Rework the cargo flash commands to specifically use the debug probe
      connected to the first RPI (there are two in the system now)

---
+++ tests/control_power_supply.py
@@ -0,0 +1,28 @@
+import argparse
+import pyvisa
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('query', help='Query to send to power supply')
+    args = parser.parse_args()
+
+    # udev creates the /dev/ttyPowerSupply symlink but pyvisa doesn't
+    # work directly with the symlink so we have to find out the actual
+    # /dev/ entry the symlink points to
+    device=Path('/dev/ttyPowerSupply').resolve()
+
+    rm = pyvisa.ResourceManager()
+    rnd_320_ka3305p = rm.open_resource('ASRL' + str(device))
+
+    # Some of the commands we use don't return anything so just handle
+    # them like this for now.
+    if args.query.startswith('VSET') or args.query.startswith('OUT'):
+        rnd_320_ka3305p.write(args.query)
+    else:
+        print(rnd_320_ka3305p.query(args.query))
+
+if __name__ == '__main__':
+    main()
+
+# TODO: Implement Python class for handling all power supply commands
